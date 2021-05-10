edfa4f9ed446b6a84ca3c308cbdc069445d2d866
Guti <gustavo.costanti.lara@gmail.com>
2021-05-10 13:14:06 -0300

    01 - exercicio 1

---
+++ src/cap4EstruturaSequencial/exercicio1.java
@@ -0,0 +1,39 @@
+package cap4EstruturaSequencial;
+
+import java.util.Locale;
+
+public class exercicio1 {
+
+	public static void main(String[] args) {
+		/*
+		Exercício: Concatenação de variáveis
+		A partir das variáveis abaixo, faça um print na tela com o seguinte output:
+		Products:
+		Computer, which price is $ 2100,00
+		Office desk, which price is $ 650,50
+
+		Record: 30 years old, code 5290 and gender: F
+
+		Measure with eight decimal places: 53,23456700
+		Rounded (three decimal places): 53,235
+		US decimal point: 53.235
+		*/
+		String product1 = "Computer";
+		String product2 = "Office desk";
+		int age = 30;
+		int code = 5290;
+		char gender = 'F';
+		double price1 = 2100.0;
+		double price2 = 650.50;
+		double measure = 53.234567;
+		
+		System.out.println("Products:");
+		System.out.printf("%s, which price is $ %.2f%n", product1, price1);
+		System.out.printf("%s, which price is $ %.2f%n", product2, price2);
+		System.out.printf("%nRecord: %d years old, code %d and gender: %c%n", age, code, gender);
+		System.out.printf("%nMeasure with eight decimal places: %.8f%n", measure);
+		System.out.printf("Rounded (three decimal places): %.3f%n", measure);
+		Locale.setDefault(Locale.US);
+		System.out.printf("US decimal point: %.3f", measure);
+	}
+}
